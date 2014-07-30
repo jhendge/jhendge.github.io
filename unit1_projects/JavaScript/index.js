b6a49766edf55893614b954a524d737fd2dc1e4a
Joseph Hendele <josephhendele@gmail.com>
2014-07-30 17:04:17 -0500

    JS testing 5

--- unit1_projects/JavaScript/index.js
+++ unit1_projects/JavaScript/index.js
@@ -1,17 +1,33 @@
-//STICKY NAV
-$(document).ready(function () {
-  var top = $('#top-line').offset().top - parseFloat($('#top-line').css('marginTop').replace(/auto/, 100));
-  $(window).scroll(function (event) {
-    // what the y position of the scroll is
-    var y = $(this).scrollTop();
+// //STICKY NAV
+// $(document).ready(function () {
+//   var top = $('#top-line').offset().top - parseFloat($('#top-line').css('marginTop').replace(/auto/, 100));
+//   $(window).scroll(function (event) {
+//     // what the y position of the scroll is
+//     var y = $(this).scrollTop();
 
-    // whether that's below the form
-    if (y >= top) {
-      // if so, add the fixed class
-      $('#top-line').addClass('fixed');
-    } else {
-      // otherwise remove it
-      $('#top-line').removeClass('fixed');
-    }
+//     // whether that's below the form
+//     if (y >= top) {
+//       // if so, add the fixed class
+//       $('#top-line').addClass('fixed');
+//     } else {
+//       // otherwise remove it
+//       $('#top-line').removeClass('fixed');
+//     }
+//   });
+// });
+
+
+$(document).ready(function(){
+
+$('li').on('mouseover', function(e){
+     e.preventDefault();
+    $(this).attr('src', 'http://phenomena.nationalgeographic.com/files/2013/01/Chinese_mantis2.jpg');
   });
+
+$('li').on('mouseout', function(e){
+     e.preventDefault();
+    $(this).attr('src', 'dbc_logo.jpg');
+  });
+
 });
+
