318e4f4800d9b605c577e62ed586234b749bda50
Joseph Hendele <josephhendele@gmail.com>
2014-07-30 17:57:06 -0500

    JS testing 8

--- unit1_projects/JavaScript/index.js
+++ unit1_projects/JavaScript/index.js
@@ -26,7 +26,22 @@ $('img').on('mouseover', function(e){
 
 $('img').on('mouseout', function(e){
      e.preventDefault();
-    $(this).attr('src', 'dbc_logo.jpg');
+    $(this).attr('src', 'http://jhendge.github.io/unit1_projects/images/Fsq_Day.jpg');
+  });
+
+  var top = $('#top-line').offset().top - parseFloat($('#top-line').css('marginTop').replace(/auto/, 100));
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >= top) {
+      // if so, add the fixed class
+      $('#top-line').addClass('fixed');
+    } else {
+      // otherwise remove it
+      $('#top-line').removeClass('fixed');
+    }
   });
 
 });
