78cdddf55910c81f6e0c2860d04e4b2a09f3ffcc
Joseph Hendele <josephhendele@gmail.com>
2014-07-30 21:41:48 -0500

    Silence JS

--- unit1_projects/JavaScript/index.js
+++ unit1_projects/JavaScript/index.js
@@ -19,30 +19,15 @@
 
 $(document).ready(function(){
 
-$('img').on('mouseover', function(e){
-     e.preventDefault();
-    $(this).attr('src', 'http://phenomena.nationalgeographic.com/files/2013/01/Chinese_mantis2.jpg');
-  });
+// $('img').on('mouseover', function(e){
+//      e.preventDefault();
+//     $(this).attr('src', 'http://phenomena.nationalgeographic.com/files/2013/01/Chinese_mantis2.jpg');
+//   });
 
-$('img').on('mouseout', function(e){
-     e.preventDefault();
-    $(this).attr('src', 'http://jhendge.github.io/unit1_projects/images/Fsq_Day.jpg');
-  });
-
-  var top = $('#top-line').offset().top - parseFloat($('#top-line').css('marginTop').replace(/auto/, 100));
-  $(window).scroll(function (event) {
-    // what the y position of the scroll is
-    var y = $(this).scrollTop();
-
-    // whether that's below the form
-    if (y >= top) {
-      // if so, add the fixed class
-      $('#top-line').addClass('fixed');
-    } else {
-      // otherwise remove it
-      $('#top-line').removeClass('fixed');
-    }
-  });
+// $('img').on('mouseout', function(e){
+//      e.preventDefault();
+//     $(this).attr('src', 'http://jhendge.github.io/unit1_projects/images/Fsq_Day.jpg');
+//   });
 
 });
 
